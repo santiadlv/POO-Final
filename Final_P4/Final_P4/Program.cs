d59947be0d39d9e28d83a3d0f9989f65b619a420
santiadlv <49079265+santiadlv@users.noreply.github.com>
2019-05-16 12:05:07 -0600

    Final Release, Implementation Complete

--- Final_P4/Final_P4/Program.cs
+++ Final_P4/Final_P4/Program.cs
@@ -11,7 +11,35 @@ namespace Final_P4
     {
         static void Main(string[] args)
         {
-            
+            int[] arr = { 9, 5, 4, 6, 8, 3, 1, 7, 2 };
+
+            foreach (int x in arr)
+            {
+                Console.WriteLine(x);
+            }
+
+            SelectionSort.selectionSort(arr);
+
+            foreach (int x in arr)
+            {
+                Console.WriteLine(x);
+            }
+
+            string[] word = { "hola", "adios", "elefante", "delfin", "hoz" };
+
+            foreach (string x in word)
+            {
+                Console.WriteLine(x);
+            }
+
+            SelectionSortG.selectionSort(word);
+
+            foreach (string x in word)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadKey();
         }
     }
 }
