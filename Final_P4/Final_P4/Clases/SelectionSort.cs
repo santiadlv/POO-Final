b58172842cd75c678dad890a434f5e062e249e8c
santiadlv <49079265+santiadlv@users.noreply.github.com>
2019-05-16 12:03:12 -0600

    Initial Commit

---
+++ Final_P4/Final_P4/Clases/SelectionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_P4.Clases
+{
+    class SelectionSort
+    {
+        public static void selectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int min = array[i];
+                int minId = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < min)
+                    {
+                        min = array[j];
+                        minId = j;
+                    }
+                }
+                // swapping
+                int temp = array[i];
+                array[i] = min;
+                array[minId] = temp;
+            }
+        }
+    }
+}
---
+++ Final_P4/Final_P4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Final_P4.Clases;
+
+namespace Final_P4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
