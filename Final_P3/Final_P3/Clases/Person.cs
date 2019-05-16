754fd591f023c1ea656b395d29d8d8a7f1cba402
santiadlv <49079265+santiadlv@users.noreply.github.com>
2019-05-16 11:27:19 -0600

    Initial Commit

---
+++ Final_P3/Final_P3/Clases/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_P3.Clases
+{
+    class Person
+    {
+        public static string name;
+        public static string lastname;
+
+        public Person(string name, string lastname)
+        {
+            
+        }
+
+        public static void askName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("\nPlease enter your last name: ");
+            lastname = Console.ReadLine();            
+        }
+    }
+}
